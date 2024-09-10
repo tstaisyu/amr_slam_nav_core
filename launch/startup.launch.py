26cd23ef3111cb8f6609d9b825a5e0ad92381ace
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-09-10 17:19:14 +0900

    Create a file

---
+++ launch/startup.launch.py
@@ -0,0 +1,9 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    return LaunchDescription([
+
+    ])
