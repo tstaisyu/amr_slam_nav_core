df5eadf428f34731333ab80f54764cbe411de566
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-09-18 21:59:03 +0900

    Edit params temporary

--- launch/startup.launch.py
+++ launch/startup.launch.py
@@ -150,7 +150,7 @@ def generate_launch_description():
         package='tf2_ros',
         executable='static_transform_publisher',
         name='static_transform_publisher_odom_to_base_link',
-        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '0.0', '1.0', 'odom', 'base_link'],
+        arguments=['0.0', '0.0', '0.0', '0.0', '0.0', '1.0', '0.0', 'odom', 'base_link'],
         output='screen'
     )
 
