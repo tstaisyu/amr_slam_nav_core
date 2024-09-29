a8af1ebe26fa2b07445561d4a2af8ebdf92ef2da
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-09-29 11:35:49 +0900

    Edit

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -7,4 +7,5 @@ source /home/<your-username>/neuratruck_ws/install/setup.bash
 # Execute ROS 2 launch file
 ros2 launch amr_slam_nav_core startup.launch.py
 
+
 ros2 launch nav2_bringup bringup_launch.py  map:=$HOME/map/home_ts_corridor/my_saved_map.yaml params_file:=$HOME/neuratruck_ws/src/amr_slam_nav_core/config/nav2_params.yaml
