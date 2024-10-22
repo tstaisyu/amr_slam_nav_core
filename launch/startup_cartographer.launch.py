bece33b47c59e4a55985eb803c64a84ea9629442
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-10-23 04:53:04 +0900

    Add imu0 settings to ekf node parameters

--- launch/startup_cartographer.launch.py
+++ launch/startup_cartographer.launch.py
@@ -138,7 +138,7 @@ def generate_launch_description():
         executable='ekf_node',
         name='ekf_localization_node',
         output='screen',
-        parameters=[{'/home/taisyu/neuratruck_ws/src/amr_slam_nav_core/config/ekf_config.yaml'}, {'use_sim_time': use_sim_time}],
+        parameters=[ekf_config, {'imu0': '/imu/data'}],
         remappings=[
            # ('/odom', '/odom_cartographer'),
            # ('/imu/data', '/left_wheel_imu')  # Remap as necessary
