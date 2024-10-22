2afe10abb8c60ab209cc2f2464bdc526e56814f4
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-10-23 08:38:37 +0900

    Edit imu topic pass

--- launch/startup_cartographer.launch.py
+++ launch/startup_cartographer.launch.py
@@ -139,11 +139,11 @@ def generate_launch_description():
         executable='ekf_node',
         name='ekf_localization_node',
         output='screen',
-        parameters=[ekf_config, {'imu0: /imu/data'}],
+        parameters=[ekf_config, {'imu0: imu/data'}],
         arguments=['--ros-args', '--log-level', 'debug'],        
         remappings=[
             ('/odom', '/odom_cartographer'),
-            ('/imu/data', '/imu/data')  # Remap as necessary
+            ('imu/data', 'imu/data')  # Remap as necessary
         ]
     )
 
