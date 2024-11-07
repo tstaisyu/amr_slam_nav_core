b65d8c8392ce4cd71a6eb0e0510d36d2789c68e6
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-11-07 15:56:43 +0900

    Add a publisher to send a request to check the connection and a subscriber to receive the response of the connection establishment

--- src/connection_checker.cpp
+++ src/connection_checker.cpp
@@ -13,3 +13,49 @@
  * limitations under the License.
  */
 
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+
+class ConnectionChecker : public rclcpp::Node
+{
+public:
+  ConnectionChecker()
+  : Node("connection_checker")
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::Int32>("connection_check_request", 10);
+    left_wheel_subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+      "/left_wheel/connection_response", 10, [this](std_msgs::msg::Int32::SharedPtr msg) {
+        if (msg->data == 1) {
+          RCLCPP_INFO(this->get_logger(), "left_wheel connected.");
+        }
+      });
+    right_wheel_subscription_ = this->create_subscription<std_msgs::msg::Int32>(
+      "/right_wheel/connection_response", 10, [this](std_msgs::msg::Int32::SharedPtr msg) {
+        if (msg->data == 1) {
+          RCLCPP_INFO(this->get_logger(), "right_wheel connected.");
+        }
+      });
+
+    timer_ = this->create_wall_timer(
+      std::chrono::seconds(1), [this]() {
+        std_msgs::msg::Int32 msg;
+        msg.data = 1;
+        publisher_->publish(msg);
+      });
+  }
+
+private:
+  rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr publisher_;
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr left_wheel_subscription_;
+  rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr right_wheel_subscription_;
+  rclcpp::TimerBase::SharedPtr timer_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<ConnectionChecker>();
+  rclcpp::spin(node);
+  rclcpp::shutdown();
+  return 0;
+}
