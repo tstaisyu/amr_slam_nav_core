18901683dc677846ae907200406661ae95efd691
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-11-12 01:47:06 +0900

    Add teleop twist joy node

--- launch/teleop.launch.py
+++ launch/teleop.launch.py
@@ -11,3 +11,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    package_name = 'amr_slam_nav_core'
+    package_dir = get_package_share_directory(package_name)
+    config_file = os.path.join(package_dir, 'config', 'joy_teleop_config.yaml')
+
+    teleop_twist_joy_node = Node(
+        package='teleop_twist_joy',
+        executable='teleop_node',
+        name='teleop_twist_joy',
+        parameters=[config_file],
+        remappings=[
+            ('cmd_vel', 'cmd_vel')
+        ],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        teleop_twist_joy_node
+    ])
