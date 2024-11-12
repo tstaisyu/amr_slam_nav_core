ac055ac047e5f6402ec5e6c71a196e4bb77827d8
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-11-13 03:05:02 +0900

    Edit including module

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -14,9 +14,9 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, Shutdown
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
