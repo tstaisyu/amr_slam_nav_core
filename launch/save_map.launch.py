3f3cea6f244df66d851bcbf4493d4038f996be96
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-11-12 23:20:18 +0900

    Discard the save_map function and add an event handler to execute the conversion process after the write_state service call is completed

--- launch/save_map.launch.py
+++ launch/save_map.launch.py
@@ -14,52 +14,10 @@
 
 import os
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, LogInfo, OpaqueFunction
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, LogInfo, RegisterEventHandler
 from launch.substitutions import LaunchConfiguration
+from launch.event_handlers import OnProcessExit
 from ament_index_python.packages import get_package_share_directory
-
-def save_map(context, *args, **kwargs):
-    map_name = context.perform_substitution(LaunchConfiguration('map_name'))
-    home_dir = os.path.expanduser('~')
-    map_dir = os.path.join(home_dir, 'maps')
-
-    # マップディレクトリの作成
-    os.makedirs(map_dir, exist_ok=True)
-
-    # pbstreamファイルのパス
-    pbstream_file = os.path.join(map_dir, f'{map_name}.pbstream')
-
-    # ROSマップファイルのパス
-    ros_map_stem = os.path.join(map_dir, map_name)
-
-    # `/write_state` サービスの呼び出し
-    call_write_state = [
-        'ros2', 'service', 'call', '/write_state', 'cartographer_ros_msgs/srv/WriteState',
-        f'{{"filename": "{pbstream_file}", "include_unfinished_submaps": true}}'
-    ]
-
-    # pbstreamからROSマップへの変換
-    convert_pbstream = [
-        'ros2', 'run', 'cartographer_ros', 'cartographer_pbstream_to_ros_map',
-        f'-map_filestem={ros_map_stem}',
-        f'-pbstream_filename={pbstream_file}',
-        '-resolution=0.05'
-    ]
-
-    # マップ保存の実行
-    return [
-        ExecuteProcess(
-            cmd=call_write_state,
-            output='screen'
-        ),
-        ExecuteProcess(
-            cmd=convert_pbstream,
-            output='screen'
-        ),
-        LogInfo(
-            msg=[f'Map saved as {ros_map_stem}.yaml and {ros_map_stem}.pgm in {map_dir}']
-        )
-    ]
 
 def generate_launch_description():
     ld = LaunchDescription()
@@ -72,8 +30,52 @@ def generate_launch_description():
     )
     ld.add_action(map_name_arg)
 
-    # OpaqueFunctionを使用してマップ保存処理を実行
-    save_map_action = OpaqueFunction(function=save_map)
-    ld.add_action(save_map_action)
+    # Launch引数の取得
+    map_name = LaunchConfiguration('map_name')
+
+    # マップ保存先のディレクトリとファイルパスの設定
+    home_dir = os.path.expanduser('~')
+    map_dir = os.path.join(home_dir, 'maps')
+    pbstream_file = os.path.join(map_dir, f'{map_name}.pbstream')
+    ros_map_stem = os.path.join(map_dir, map_name)
+
+    # マップディレクトリの作成
+    mkdir = ExecuteProcess(
+        cmd=['mkdir', '-p', map_dir],
+        output='screen'
+    )
+    ld.add_action(mkdir)
+
+    # `/write_state` サービスの呼び出し
+    call_write_state = ExecuteProcess(
+        cmd=[
+            'ros2', 'service', 'call', '/write_state',
+            'cartographer_ros_msgs/srv/WriteState',
+            f'{{"filename": "{pbstream_file}", "include_unfinished_submaps": true}}'
+        ],
+        output='screen',
+        shell=True
+    )
+
+    # pbstreamからROSマップへの変換コマンド
+    convert_map = ExecuteProcess(
+        cmd=[
+            'ros2', 'run', 'cartographer_ros', 'cartographer_pbstream_to_ros_map',
+            f'-map_filestem={ros_map_stem}',
+            f'-pbstream_filename={pbstream_file}',
+            '-resolution=0.05'
+        ],
+        output='screen'
+    )
+
+    # サービス呼び出し完了後に変換プロセスを実行するイベントハンドラー
+    save_map_event_handler = RegisterEventHandler(
+        OnProcessExit(
+            target_action=call_write_state,
+            on_exit=[convert_map, LogInfo(msg=[f'Map saved as {ros_map_stem}.yaml and {ros_map_stem}.pgm in {map_dir}'])]
+        )
+    )
+    ld.add_action(call_write_state)
+    ld.add_action(save_map_event_handler)
 
     return ld
