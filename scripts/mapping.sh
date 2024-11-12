26fd5eea3c6c7e4edc90ceb0a6e07b21341e6e50
taisyu shibata <t_shiba117@yahoo.co.jp>
2024-11-13 01:37:13 +0900

    Update

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -5,7 +5,9 @@ source ~/ros2_ws/install/setup.bash
 source ~/neuratruck_ws/install/setup.bash
 
 # Execute ROS 2 launch file
-ros2 launch amr_slam_nav_core startup_cartographer.launch.py
+ros2 launch amr_slam_nav_core startup.launch.py
 
+delay=2
 
-# ros2 launch nav2_bringup bringup_launch.py  map:=$HOME/map/home_ts_corridor/my_saved_map.yaml params_file:=$HOME/neuratruck_ws/src/amr_slam_nav_core/config/nav2_params.yaml
+# Start mapping
+ros2 launch amr_slam_nav_core mapping.launch.py
